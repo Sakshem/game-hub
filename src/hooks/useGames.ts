fcaedb402f171113ce3341381535392498a65c60
Sakshem <sakshemjain7960@gmail.com>
2023-06-28 22:00:06 +0000

    filter items by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,5 +12,11 @@ export interface Game {
     parent_platforms: { platform : Platform }[]
     metacritic: number;
 }
-const useGames = (selectedGenre : Genre | null) => useData<Game>('/games', { params: { genres: selectedGenre?.id}}, [selectedGenre?.id]);
+const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => 
+useData<Game>('/games', { 
+  params: { 
+    genres: selectedGenre?.id, platforms: selectedPlatform?.id 
+  }}, 
+  [ selectedGenre?.id, selectedPlatform?.id ]
+);
 export default useGames;
