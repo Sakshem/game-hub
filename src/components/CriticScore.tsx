13a342b8fb2778f1aef93c115f004a659199a286
Sakshem <sakshemjain7960@gmail.com>
2023-06-28 13:35:33 +0000

    Get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -3,6 +3,7 @@ import { Badge } from '@chakra-ui/react';
 interface Props {
     score: number;
 }
+
 const CriticScore = ({score} : Props) => {
   let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
   return (
