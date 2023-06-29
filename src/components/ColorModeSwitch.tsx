f98271ab16b95a6242c8bdc65201d4ec43cd9b1a
Sakshem <sakshemjain7960@gmail.com>
2023-06-29 18:47:45 +0000

    dynamic text for dark & light mode

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,10 +1,11 @@
 import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
+  console.log(colorMode);
   return (
     <HStack>
         <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
-        <Text whiteSpace='nowrap'>Dark Mode</Text>
+        <Text whiteSpace='nowrap'>{colorMode.charAt(0).toUpperCase() + colorMode.slice(1).toLowerCase()} mode</Text>
     </HStack>
   )
 }
