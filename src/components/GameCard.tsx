31472c492aa3fc6c7d6a162f6bdb3a9eca81fe3b
Sakshem <sakshemjain7960@gmail.com>
2023-06-29 18:53:25 +0000

    Improved card UI for light mode

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 const GameCard = ({ game } : Props) => {
   return (
-    <Card height='100%'>
+    <Card height='100%' variant='filled'>
         <Image src={getCroppedImageUrl(game.background_image)}/>
         <CardBody>
             <HStack justifyContent='space-between' marginBottom={3}>
